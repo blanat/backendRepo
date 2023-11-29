07515b9710b0b8d700e47cb0ceeeb9c7f5c995f3
oumaimaBens <imiasumibenz@gmail.com>
2023-11-29 18:31:24 +0100

    DataTesting

--- Blanat/src/main/java/Ensak/Blanat/Blanat/BlanatApplication.java
+++ Blanat/src/main/java/Ensak/Blanat/Blanat/BlanatApplication.java
@@ -1,7 +1,11 @@
 package Ensak.Blanat.Blanat;
 
 import Ensak.Blanat.Blanat.entities.Client;
+import Ensak.Blanat.Blanat.entities.Comment;
+import Ensak.Blanat.Blanat.entities.Deal;
 import Ensak.Blanat.Blanat.entities.Moderator;
+import Ensak.Blanat.Blanat.repositories.CommentRepository;
+import Ensak.Blanat.Blanat.repositories.DealRepository;
 import Ensak.Blanat.Blanat.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 @SpringBootApplication
 public class BlanatApplication {
@@ -18,10 +23,8 @@ public class BlanatApplication {
 	}
 
 	@Bean
-	public CommandLineRunner initData(UserRepository userRepository) {
+	public CommandLineRunner initDealAndCommentData(DealRepository dealRepository, CommentRepository commentRepository, UserRepository userRepository) {
 		return args -> {
-
-
 			// Create and save a Client
 			Client client = new Client();
 			client.setUsername("client1");
@@ -38,6 +41,29 @@ public class BlanatApplication {
 			moderator.setDate_Adherence(LocalDate.now());
 			moderator.setSuperAdmin(true);
 			userRepository.save(moderator);
+
+			// Create and save a Deal
+			Deal deal = new Deal();
+			deal.setTitre("Deal 1");
+			deal.setDescription("This is the first deal");
+			deal.setDate_debut(new Date());
+			deal.setDate_fin(new Date());
+			deal.setPrix(50.0f);
+			deal.setLocalisation("Some location");
+			deal.setLivraison_prix(5.0f);
+			deal.setVote_up(0);
+			deal.setVote_down(0);
+			deal.setDateCreation(LocalDate.now());
+			dealRepository.save(deal);
+
+			// Create and save a Comment for the Deal by the Client
+			Comment comment = new Comment();
+			comment.setDate(new Date());
+			comment.setContent("This is a comment for Deal 1");
+			comment.setUser(client);
+			comment.setDeal(deal);
+			commentRepository.save(comment);
 		};
 	}
+
 }
