e013e9fc7cfbe4a20b37f65de62c3ed57c394434
oumaimaBens <imiasumibenz@gmail.com>
2023-12-02 13:26:49 +0100

    testpipline

--- Blanat/src/main/java/Ensak/Blanat/Blanat/BlanatApplication.java
+++ Blanat/src/main/java/Ensak/Blanat/Blanat/BlanatApplication.java
@@ -2,6 +2,8 @@ package Ensak.Blanat.Blanat;
 
 import Ensak.Blanat.Blanat.entities.Comment;
 import Ensak.Blanat.Blanat.entities.Deal;
+import Ensak.Blanat.Blanat.entities.UserApp;
+import Ensak.Blanat.Blanat.enums.Categories;
 import Ensak.Blanat.Blanat.repositories.CommentRepository;
 import Ensak.Blanat.Blanat.repositories.DealRepository;
 import Ensak.Blanat.Blanat.repositories.UserRepository;
@@ -12,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication
 public class BlanatApplication {
@@ -20,11 +23,7 @@ public class BlanatApplication {
 		SpringApplication.run(BlanatApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner initDealAndCommentData(DealRepository dealRepository, CommentRepository commentRepository, UserRepository userRepository) {
-		return args -> {
 
-		};
-	}
+	
 
 }
