ef5a2ef8ecc95adfe57f9e2a7947cd073ed2bf8b
Chigaw <amhex01@gmail.com>
2023-12-19 19:15:02 +0100

    Update UserController.java

--- blanat/src/main/java/Ensak/Blanat/Blanat/controllers/authControllers/UserController.java
+++ blanat/src/main/java/Ensak/Blanat/Blanat/controllers/authControllers/UserController.java
@@ -2,9 +2,7 @@ package Ensak.Blanat.Blanat.controllers.authControllers;
 
 import Ensak.Blanat.Blanat.entities.UserApp;
 import Ensak.Blanat.Blanat.services.authServices.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,16 @@ public class UserController {
     @GetMapping
     public List<UserApp> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @PutMapping("/{email}/password")
+    public UserApp updatePassword(@PathVariable String email, @RequestBody String newPassword) {
+        return userService.updatePassword(email, newPassword);
+    }
+
+    @DeleteMapping("/{email}")
+    public void deleteUser(@PathVariable String email) {
+        userService.deleteUser(email);
     }
 
     // You can add more methods for handling other user-related operations
