da432ae5754ec024dbfc873a16b27301aaf7b25a
Chigaw <amhex01@gmail.com>
2023-12-21 21:19:27 +0100

    modified security config so notification gets sent

--- blanat/src/main/java/Ensak/Blanat/Blanat/BlanatApplication.java
+++ blanat/src/main/java/Ensak/Blanat/Blanat/BlanatApplication.java
@@ -1,9 +1,17 @@
 package Ensak.Blanat.Blanat;
 
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableJpaRepositories("Ensak.Blanat.Blanat.repositories")
--- blanat/src/main/java/Ensak/Blanat/Blanat/config/notifconfig/FirebaseInitializer.java
+++ blanat/src/main/java/Ensak/Blanat/Blanat/config/notifconfig/FirebaseInitializer.java
@@ -1,14 +1,17 @@
-package Ensak.Blanat.Blanat.services.notificationService;
+package Ensak.Blanat.Blanat.config.notifconfig;
+
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 
+@Configuration
 public class FirebaseInitializer {
 
     @Bean
@@ -17,8 +20,9 @@ public class FirebaseInitializer {
                 .fromStream(new ClassPathResource("firebase-service-account.json").getInputStream());
         FirebaseOptions firebaseOptions = FirebaseOptions.builder()
                 .setCredentials(googleCredentials).build();
-        FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "my-app");
+        FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "blanat");
         return FirebaseMessaging.getInstance(app);
     }
 }
+
 
