c8b92c37d52250e2118eeb3619e541dfa729d9b3
Chigaw <amhex01@gmail.com>
2023-12-21 18:51:06 +0100

    adding notification to backend

---
+++ blanat/src/main/java/Ensak/Blanat/Blanat/controllers/notificationController/NotificationController.java
@@ -0,0 +1,22 @@
+package Ensak.Blanat.Blanat.controllers.notificationController;
+
+import Ensak.Blanat.Blanat.services.notificationService.FirebaseMessagingService;
+import Ensak.Blanat.Blanat.services.notificationService.NotificationMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notification")
+public class NotificationController {
+
+    @Autowired
+    FirebaseMessagingService firebaseMessagingService;
+
+    @PostMapping
+    public String sendNotificationByToken(@RequestBody NotificationMessage notificationMessage){
+        return firebaseMessagingService.sendNotificationByToken(notificationMessage);
+    }
+}
