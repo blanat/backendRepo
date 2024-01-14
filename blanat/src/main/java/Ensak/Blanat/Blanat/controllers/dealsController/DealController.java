ee8dcd9a16e2cb2dcaa09d620080cc545b3047b1
ChaimaaLou <louadnichaima@gmail.com>
2024-01-14 02:29:45 +0100

    retrieve deal by user email

--- blanat/src/main/java/Ensak/Blanat/Blanat/controllers/dealsController/DealController.java
+++ blanat/src/main/java/Ensak/Blanat/Blanat/controllers/dealsController/DealController.java
@@ -5,7 +5,9 @@ import Ensak.Blanat.Blanat.DTOs.dealDTO.ListDealDTO;
 import Ensak.Blanat.Blanat.entities.Deal;
 import Ensak.Blanat.Blanat.entities.ImagesDeal;
 import Ensak.Blanat.Blanat.entities.UserApp;
+import Ensak.Blanat.Blanat.exeptions.UserNotFoundException;
 import Ensak.Blanat.Blanat.mappers.DealMapper;
+import Ensak.Blanat.Blanat.repositories.UserRepository;
 import Ensak.Blanat.Blanat.services.imagesDealService.imagesServiceInterface;
 import Ensak.Blanat.Blanat.services.authServices.UserService;
 import Ensak.Blanat.Blanat.services.dealService.DealServiceInterface;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/deals")
 public class DealController {
@@ -26,6 +30,8 @@ public class DealController {
     private final UserService userService;
     private final DealMapper dealMapper;
 
+    @Autowired
+    private UserRepository userRepository;
     @Autowired
     public DealController(DealServiceInterface dealService, UserService userService, DealMapper dealMapper,imagesServiceInterface imageService) {
         this.dealService = dealService;
@@ -90,6 +96,24 @@ public class DealController {
     }
 
     //==================================
+    @GetMapping("/userByEmail/{email}")
+    public ResponseEntity<List<ListDealDTO>> getListDealsDTOByUserEmail(@PathVariable("email") String email) {
+        try {
+            Optional<Long> userIdOptional = userRepository.findUserIdByEmail(email);
+
+            if (userIdOptional.isPresent()) {
+                List<ListDealDTO> listDealDTOs = dealService.getListDealsDTOByUserId(userIdOptional.get());
+                return ResponseEntity.ok(listDealDTOs);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
 
 /*
 
