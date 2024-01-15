e87d7a680c0a87db2e82e7e7682d9c1372104553
youssef-zeaikor <ussef199801@gmail.com>
2024-01-15 01:05:03 +0100

    add image & date joined

--- blanat/src/main/java/Ensak/Blanat/Blanat/DTOs/ethDoa/ProfileDTO.java
+++ blanat/src/main/java/Ensak/Blanat/Blanat/DTOs/ethDoa/ProfileDTO.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @Data
 @Getter
@@ -14,5 +17,10 @@ public class ProfileDTO {
     String userName;
 
     String email;
+
+    LocalDateTime createdAt;
+    
+    String profileFilePath;
+
 
 }
